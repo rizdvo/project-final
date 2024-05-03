6339b7485b191fbfa40a9cefc7de87b8df78eba9
bohdan <bohdanvasilev2017@gmail.com>
2024-05-03 19:41:03 +0300

    Delete all code in places where use vk, yandex

--- src/main/resources/db/changelog.sql
+++ src/main/resources/db/changelog.sql
@@ -218,7 +218,6 @@ values ('task', 'Task', 2),
        ('mobile', 'Mobile', 0),
        ('phone', 'Phone', 0),
        ('website', 'Website', 0),
-       ('vk', 'VK', 0),
        ('linkedin', 'LinkedIn', 0),
        ('github', 'GitHub', 0),
 -- PRIORITY
@@ -326,6 +325,5 @@ values ('todo', 'ToDo', 3, 'in_progress,canceled|'),
        ('canceled', 'Canceled', 3, null);
 
 --changeset ishlyakhtenkov:change_UK_USER_BELONG
-
 drop index UK_USER_BELONG;
 create unique index UK_USER_BELONG on USER_BELONG (OBJECT_ID, OBJECT_TYPE, USER_ID, USER_TYPE_CODE) where ENDPOINT is null;
